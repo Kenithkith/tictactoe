a4f2e0bb3a3200627dd359ed222eb570a3618651
Patryk Ostrowski <ostrowski.patryk@gmail.com>
2016-08-09 21:58:03 +0200

    Checking if given field wasn't booked and looping if so.

--- Game/src/Circle.java
+++ Game/src/Circle.java
@@ -1,5 +1,3 @@
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 /**
@@ -11,65 +9,73 @@ public class Circle {
     boolean chosenField;
 
     void getNumberFromCircle() {
-        System.out.println("Circle, choose the field...");
+        chosenField = false;
 
-        tile = scanner.nextLine();
-        if (tile.equals("1") || tile.equals("2") || tile.equals("3") || tile.equals("4") || tile.equals("5") || tile.equals("6") || tile.equals("7") || tile.equals("8") || tile.equals("9")) {
-            chosenField = true;
+        round:
+        {
+            System.out.println("Circle, choose the field...");
+            tile = scanner.nextLine();
+            int parsedTile = Integer.parseInt(tile);
+
+            error:
+            {
+                if (Pulpit.results[parsedTile - 1] == 200 || Pulpit.results[parsedTile - 1] == 100) {
+                    System.out.println("This tile is already taken.");
+                    break error;
+                }
+
+                if (tile.equals("1") || tile.equals("2") || tile.equals("3") || tile.equals("4") || tile.equals("5") || tile.equals("6") || tile.equals("7") || tile.equals("8") || tile.equals("9")) {
+                    chosenField = true;
+                }
+
+                System.out.println("Your choice: " + tile);
+
+                switch (tile) {
+                    case "1":
+                        Pulpit.results[0] = 100;
+                        break;
+                    case "2":
+                        Pulpit.results[1] = 100;
+                        break;
+                    case "3":
+                        Pulpit.results[2] = 100;
+                        break;
+                    case "4":
+                        Pulpit.results[3] = 100;
+                        break;
+                    case "5":
+                        Pulpit.results[4] = 100;
+                        break;
+                    case "6":
+                        Pulpit.results[5] = 100;
+                        break;
+                    case "7":
+                        Pulpit.results[6] = 100;
+                        break;
+                    case "8":
+                        Pulpit.results[7] = 100;
+                        break;
+                    case "9":
+                        Pulpit.results[8] = 100;
+                        break;
+                }
+            }
         }
-        System.out.println("Your choice: " + tile);
-
-        int parsedTile = Integer.parseInt(tile);
-        if (Pulpit.results[parsedTile - 1] == 200 || Pulpit.results[parsedTile - 1] == 100) {
-            System.out.println("This tile is already taken.");
-        }
-
-        switch (tile) {
-            case "1":
-                Pulpit.results[0] = 100;
-                break;
-            case "2":
-                Pulpit.results[1] = 100;
-                break;
-            case "3":
-                Pulpit.results[2] = 100;
-                break;
-            case "4":
-                Pulpit.results[3] = 100;
-                break;
-            case "5":
-                Pulpit.results[4] = 100;
-                break;
-            case "6":
-                Pulpit.results[5] = 100;
-                break;
-            case "7":
-                Pulpit.results[6] = 100;
-                break;
-            case "8":
-                Pulpit.results[7] = 100;
-                break;
-            case "9":
-                Pulpit.results[8] = 100;
-                break;
-        }
-
     }
 
     boolean checkIfCircleWon() {
-        if ( (Pulpit.results[0] == 100 && Pulpit.results[1] == 100 && Pulpit.results[2] == 100) ||
-        (Pulpit.results[3] == 100 && Pulpit.results[4] == 100 && Pulpit.results[5] == 100) ||
-        (Pulpit.results[6] == 100 && Pulpit.results[7] == 100 && Pulpit.results[8] == 100) ||
-        (Pulpit.results[0] == 100 && Pulpit.results[3] == 100 && Pulpit.results[6] == 100) ||
-        (Pulpit.results[1] == 100 && Pulpit.results[4] == 100 && Pulpit.results[7] == 100) ||
-        (Pulpit.results[2] == 100 && Pulpit.results[5] == 100 && Pulpit.results[8] == 100) ||
-        (Pulpit.results[0] == 100 && Pulpit.results[4] == 100 && Pulpit.results[8] == 100) ||
-        (Pulpit.results[2] == 100 && Pulpit.results[4] == 100 && Pulpit.results[6] == 100) ) {
+        if ((Pulpit.results[0] == 100 && Pulpit.results[1] == 100 && Pulpit.results[2] == 100) ||
+                (Pulpit.results[3] == 100 && Pulpit.results[4] == 100 && Pulpit.results[5] == 100) ||
+                (Pulpit.results[6] == 100 && Pulpit.results[7] == 100 && Pulpit.results[8] == 100) ||
+                (Pulpit.results[0] == 100 && Pulpit.results[3] == 100 && Pulpit.results[6] == 100) ||
+                (Pulpit.results[1] == 100 && Pulpit.results[4] == 100 && Pulpit.results[7] == 100) ||
+                (Pulpit.results[2] == 100 && Pulpit.results[5] == 100 && Pulpit.results[8] == 100) ||
+                (Pulpit.results[0] == 100 && Pulpit.results[4] == 100 && Pulpit.results[8] == 100) ||
+                (Pulpit.results[2] == 100 && Pulpit.results[4] == 100 && Pulpit.results[6] == 100)) {
             System.out.println("CIRCLE WON!");
             Pulpit.pulpitSummary();
             return true;
         }
         return false;
     }
-
 }
--- Game/src/Plus.java
+++ Game/src/Plus.java
@@ -9,49 +9,58 @@ public class Plus {
     boolean chosenField;
 
     void getNumberFromPlus() {
-        System.out.println("Plus, choose the field...");
+        chosenField = false;
 
-        tile = scanner.nextLine();
-        if (tile.equals("1") || tile.equals("2") || tile.equals("3") || tile.equals("4") || tile.equals("5") || tile.equals("6") || tile.equals("7") || tile.equals("8") || tile.equals("9")) {
-            chosenField = true;
+        round:
+        {
+            System.out.println("Plus, choose the field...");
+            tile = scanner.nextLine();
+            int parsedTile = Integer.parseInt(tile);
+
+            error:
+            {
+                if (Pulpit.results[parsedTile - 1] == 200 || Pulpit.results[parsedTile - 1] == 100) {
+                    System.out.println("This tile is already taken.");
+                    break error;
+                }
+
+                if (tile.equals("1") || tile.equals("2") || tile.equals("3") || tile.equals("4") || tile.equals("5") || tile.equals("6") || tile.equals("7") || tile.equals("8") || tile.equals("9")) {
+                    chosenField = true;
+                }
+
+                System.out.println("Your choice: " + tile);
+
+                switch (tile) {
+                    case "1":
+                        Pulpit.results[0] = 200;
+                        break;
+                    case "2":
+                        Pulpit.results[1] = 200;
+                        break;
+                    case "3":
+                        Pulpit.results[2] = 200;
+                        break;
+                    case "4":
+                        Pulpit.results[3] = 200;
+                        break;
+                    case "5":
+                        Pulpit.results[4] = 200;
+                        break;
+                    case "6":
+                        Pulpit.results[5] = 200;
+                        break;
+                    case "7":
+                        Pulpit.results[6] = 200;
+                        break;
+                    case "8":
+                        Pulpit.results[7] = 200;
+                        break;
+                    case "9":
+                        Pulpit.results[8] = 200;
+                        break;
+                }
+            }
         }
-        System.out.println("Your choice: " + tile);
-
-        int parsedTile = Integer.parseInt(tile);
-        if (Pulpit.results[parsedTile - 1] == 200 || Pulpit.results[parsedTile - 1] == 100) {
-            System.out.println("This tile is already taken.");
-        }
-
-        switch (tile) {
-            case "1":
-                Pulpit.results[0] = 200;
-                break;
-            case "2":
-                Pulpit.results[1] = 200;
-                break;
-            case "3":
-                Pulpit.results[2] = 200;
-                break;
-            case "4":
-                Pulpit.results[3] = 200;
-                break;
-            case "5":
-                Pulpit.results[4] = 200;
-                break;
-            case "6":
-                Pulpit.results[5] = 200;
-                break;
-            case "7":
-                Pulpit.results[6] = 200;
-                break;
-            case "8":
-                Pulpit.results[7] = 200;
-                break;
-            case "9":
-                Pulpit.results[8] = 200;
-                break;
-        }
-
     }
 
     boolean checkIfPlusWon() {
