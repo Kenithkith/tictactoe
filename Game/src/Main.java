830972b94c4667ac2b6f8c7f86aab1407a635859
Patryk Ostrowski <ostrowski.patryk@gmail.com>
2016-09-08 18:07:26 +0200

    Main and Pulpit commited without not necessary imports.

--- Game/src/Main.java
+++ Game/src/Main.java
@@ -1,11 +1,8 @@
-import java.util.Scanner;
-
 /**
  * Created by Patryk on 27.07.2016.
  */
 public class Main {
     public static void main(String[] args) {
-
 
         Pulpit pulpit = new Pulpit();
         do {
